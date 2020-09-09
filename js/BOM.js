1b463307238c6ba3e0c344ce588a49a66899fd80
SaulDeveloper <suarez.vega.v@gmail.com>
2020-09-09 15:26:31 +0100

    añadiendoBom

--- js/BOM.js
+++ js/BOM.js
@@ -1,10 +1,19 @@
 'use strict'
 
 // Bom o browser object model
+function nuevaUrl(cambio){
+    window.location.href = cambio;
+} 
+function ventanaNueva(nuevaVentana){
+    //window.open(nuevaVentana);
+    window.open(nuevaVentana,"","width=400,height=300");
+} 
+
 function getBom(){
     console.log(window.innerHeight); //nos da la altura de la página
     console.log(window.innerWidth); //nos da la ancho de la página
 }
+
 console.log(window.innerHeight); //nos da la altura de la página web
 console.log(window.innerWidth); //nos da la ancho de la páginaweb
 
@@ -13,3 +22,6 @@ console.log(screen.width); //nos da la ancho de la pantalla del usuario
 
 console.log(window.location); //nos muestra la url cargada (objeto location)
 console.log(window.location.href); //nos muestra la url cargada de dentro del objeto (objeto location)
+
+
+
